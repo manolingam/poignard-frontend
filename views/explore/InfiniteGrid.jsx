6b6e9d7401937688bc038d1acf720c0436c13367
Manolingam N <manolingam1996@gmail.com>
2022-04-01 21:06:27 +0530

    performance improvements

--- views/explore/InfiniteGrid.jsx
+++ views/explore/InfiniteGrid.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
-import { SimpleGrid, Box, Text, Flex, Button } from '@chakra-ui/react';
+import {
+  SimpleGrid,
+  Box,
+  Text,
+  Flex,
+  Button,
+  Image as ChakraImage
+} from '@chakra-ui/react';
 import { utils } from 'ethers';
 import Link from 'next/link';
-import Image from 'next/image';
 import styled from '@emotion/styled';
 
 import { theme } from '../../themes/theme';
+import { uriToHttp } from '../../utils/helpers';
 import { VOUCHERS_PER_PAGE, POIGNART_BUCKET_BASE_URL } from '../../config';
 
 const StyledTokenId = styled(Text)`
@@ -68,19 +75,18 @@ export const InfiniteGrid = ({ allVouchers, onlyMintable, totalPages }) => {
                 }}
                 mb='2rem'
               >
-                <Image
+                <ChakraImage
                   crossOrigin='anonymous'
                   src={`${POIGNART_BUCKET_BASE_URL}/${voucher.metadata.image.replace(
                     'ipfs://',
                     ''
                   )}`}
-                  priority={true}
-                  // loader={() => uriToHttp(voucher.metadata.image)}
+                  fallbackSrc={() => uriToHttp(voucher.metadata.image)}
                   alt='minted nft'
                   width='100%'
                   height='100%'
                   objectFit='cover'
-                  layout='responsive'
+                  loading='eager'
                 />
 
                 <Box
